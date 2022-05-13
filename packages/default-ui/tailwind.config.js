88a5337371a380c913c38d80aaa55b8a07b0aaea
Felix Maury <femaury@student.42.fr>
2022-05-13 14:39:43 +0200

    :art: Extended tailwind config with ArSnap colours

--- packages/default-ui/tailwind.config.js
+++ packages/default-ui/tailwind.config.js
@@ -1,7 +1,29 @@
 module.exports = {
     content: ["./src/**/*.ts{x,}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                purple: {
+                    DEFAULT: "#8383D4",
+                    text: "#B5B5F5",
+                    dark: "#05053C",
+                    light: "#EFEFF9",
+                },
+                green: {
+                    success: "#3CEE99",
+                },
+                red: {
+                    error: "#FF4794",
+                },
+                orange: {
+                    warning: "#FFA756",
+                },
+                gray: {
+                    dark: "#333333",
+                    light: "#888888",
+                },
+            },
+        },
     },
     plugins: [],
 };
