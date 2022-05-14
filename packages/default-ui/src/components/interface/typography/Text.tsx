7a7da3998a70d3a3acb8320ae5e83972d4221007
Felix Maury <felix@pianity.com>
2022-05-14 16:44:55 +0200

    :sparkles: Added ui text component

---
+++ packages/default-ui/src/components/interface/typography/Text.tsx
@@ -0,0 +1,156 @@
+import {
+    getColorClass,
+    getHoverColorClass,
+    getOpacityClass,
+    getSizeClass,
+    getWeightClass,
+    Opacity,
+    TextColor,
+    TextSize,
+    TextWeight,
+} from "@/utils/tailwind";
+import { PropsWithChildren } from "react";
+
+type TextProps = {
+    /** Font weight of the text */
+    weight?: TextWeight;
+    /** Increases letter-spacing to 0.05em */
+    wider?: boolean;
+    /** Increases line height to 110% */
+    taller?: boolean;
+    /** Color of the text */
+    color?: TextColor;
+    /** Color of the text on hover */
+    hoverColor?: TextColor;
+    /** Opacity of the text */
+    opacity?: Opacity;
+    /** Font size of the text (in px) */
+    size?: TextSize;
+    /** Makes the text pulse */
+    pulse?: boolean;
+    /** Extra classes added to the element */
+    className?: string;
+};
+
+/**
+ * Default text component. Renders as a div.
+ *
+ * @remarks
+ * Use Text.span, Text.label, Text.h1, Text.h2, Text.h3, Text.p
+ * to render as a specific HTML tag.
+ */
+function Text(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <div className={className}>{props.children}</div>;
+}
+
+/**
+ * Renders text component as a span.
+ */
+function span(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <span className={className}>{props.children}</span>;
+}
+
+/**
+ * Renders text component as a label.
+ */
+function label(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <label className={className}>{props.children}</label>;
+}
+
+/**
+ * Renders text component as a p.
+ */
+function p(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <p className={className}>{props.children}</p>;
+}
+
+/**
+ * Renders text component as a h1.
+ */
+function h1(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <h1 className={className}>{props.children}</h1>;
+}
+
+/**
+ * Renders text component as a h2.
+ */
+function h2(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <h2 className={className}>{props.children}</h2>;
+}
+
+/**
+ * Renders text component as a h3.
+ */
+function h3(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <h3 className={className}>{props.children}</h3>;
+}
+
+/**
+ * Renders text component as a li.
+ */
+function li(props: PropsWithChildren<TextProps>) {
+    const className = getTextClassName(props);
+    return <li className={className}>{props.children}</li>;
+}
+
+Text.span = span;
+Text.label = label;
+Text.h1 = h1;
+Text.h2 = h2;
+Text.h3 = h3;
+Text.p = p;
+Text.li = li;
+
+export default Text;
+
+function getTextClassName({
+    weight,
+    color,
+    hoverColor,
+    size,
+    opacity,
+    taller,
+    wider,
+    pulse,
+    className,
+}: TextProps) {
+    let classNames: string[] = [];
+    if (weight) {
+        classNames.push(getWeightClass(weight));
+    }
+    if (color) {
+        classNames.push(getColorClass(color));
+    }
+    if (hoverColor) {
+        classNames.push(getHoverColorClass(hoverColor));
+    }
+    if (size) {
+        classNames.push(getSizeClass(size));
+    }
+    if (opacity) {
+        classNames.push(getOpacityClass(opacity));
+    }
+    if (taller) {
+        classNames.push("leading-[110%]");
+    } else {
+        classNames.push("leading-none");
+    }
+    if (wider) {
+        classNames.push("tracking-[0.05em]");
+    }
+    if (className) {
+        classNames.push(className);
+    }
+    if (pulse) {
+        classNames.push("animate-pulse");
+    }
+
+    return classNames.join(" ");
+}
---
+++ packages/default-ui/src/utils/tailwind.ts
@@ -0,0 +1,184 @@
+export type TextColor =
+    | "white"
+    | "gray-dark"
+    | "gray-light"
+    | "purple"
+    | "purple-light"
+    | "purple-dark"
+    | "purple-text"
+    | "green"
+    | "red"
+    | "orange";
+
+/**
+ * Gets the tailwind class for the given text color.
+ *
+ * @param color
+ * @returns Tailwind class name
+ */
+export function getColorClass(color: TextColor): string {
+    switch (color) {
+        case "white":
+            return "text-white";
+
+        case "gray-dark":
+            return "text-gray-dark";
+
+        case "gray-light":
+            return "text-gray-light";
+
+        case "purple":
+            return "text-purple";
+
+        case "purple-light":
+            return "text-purple-light";
+
+        case "purple-dark":
+            return "text-purple-dark";
+
+        case "purple-text":
+            return "text-purple-text";
+
+        case "green":
+            return "text-green-success";
+
+        case "red":
+            return "text-red-error";
+
+        case "orange":
+            return "text-orange-warning";
+    }
+}
+
+/**
+ * Gets the tailwind hover class for the given text color.
+ *
+ * @param color Tailwind text color
+ * @returns Tailwind hover class name
+ */
+export function getHoverColorClass(color: TextColor): string {
+    switch (color) {
+        case "white":
+            return "lg:hover:text-white";
+
+        case "gray-dark":
+            return "lg:hover:text-gray-dark";
+
+        case "gray-light":
+            return "lg:hover:text-gray-light";
+
+        case "purple":
+            return "lg:hover:text-purple";
+
+        case "purple-light":
+            return "lg:hover:text-purple-light";
+
+        case "purple-dark":
+            return "lg:hover:text-purple-dark";
+
+        case "purple-text":
+            return "lg:hover:text-purple-text";
+
+        case "green":
+            return "lg:hover:text-green-success";
+
+        case "red":
+            return "lg:hover:text-red-error";
+
+        case "orange":
+            return "lg:hover:text-orange-warning";
+    }
+}
+
+export type Opacity = "0" | "25" | "50" | "75" | "100";
+
+/**
+ * Gets the tailwind class for the given opacity.
+ *
+ * @param opacity
+ * @returns Tailwind class name
+ */
+export function getOpacityClass(opacity: Opacity): string {
+    switch (opacity) {
+        case "0":
+            return "opacity-0";
+
+        case "25":
+            return "opacity-25";
+
+        case "50":
+            return "opacity-50";
+
+        case "75":
+            return "opacity-75";
+
+        case "100":
+            return "opacity-100";
+    }
+}
+
+export type TextSize = "11" | "12" | "13" | "14" | "16" | "18" | "20" | "24" | "32" | "40" | "56";
+
+/**
+ * Gets the tailwind class for the given text color.
+ *
+ * @param color
+ * @returns Tailwind class name
+ */
+export function getSizeClass(size: TextSize): string {
+    switch (size) {
+        case "11":
+            return "text-[11px]";
+
+        case "12":
+            return "text-[12px]";
+
+        case "13":
+            return "text-[13px]";
+
+        case "14":
+            return "text-[14px]";
+
+        case "16":
+            return "text-[16px]";
+
+        case "18":
+            return "text-[18px]";
+
+        case "20":
+            return "text-[20px]";
+
+        case "24":
+            return "text-[24px]";
+
+        case "32":
+            return "text-[32px]";
+
+        case "40":
+            return "text-[40px]";
+
+        case "56":
+            return "text-[56px]";
+    }
+}
+
+export type TextWeight = "normal" | "semibold" | "bold";
+
+/**
+ * Gets the tailwind class for the given text weight.
+ *
+ * @param weight
+ * @returns Tailwind class name
+ */
+export function getWeightClass(weight: TextWeight) {
+    switch (weight) {
+        case "normal":
+            return "font-normal";
+
+        case "semibold":
+            return "font-semibold";
+
+        case "bold":
+            return "font-bold";
+    }
+}
