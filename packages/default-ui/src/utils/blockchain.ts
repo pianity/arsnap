143427e5fd976cb5399f22b6317be1ecbf69c082
Eyal Chojnowski <eyal.choj@gmail.com>
2022-05-16 14:33:37 +0200

    :zap: (default-ui) set gateway to Redstone's testnet

--- packages/default-ui/src/utils/blockchain.ts
+++ packages/default-ui/src/utils/blockchain.ts
@@ -1,7 +1,15 @@
 import Arweave from "arweave";
 
+// TODO: Make the gateway configurable
+
 export const arweave = Arweave.init({
-    host: "arweave.net",
+    host: "testnet.redstone.tools",
     port: 443,
     protocol: "https",
 });
+
+// export const arweave = Arweave.init({
+//     host: "arweave.net",
+//     port: 443,
+//     protocol: "https",
+// });
