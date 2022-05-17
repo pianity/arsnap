26c102e67129ae640045b33423a5b7106b743d40
Felix Maury <felix@pianity.com>
2022-05-17 09:23:06 +0200

    :zap: (default-ui) added link to send view from balance component

--- packages/default-ui/src/components/Balance.tsx
+++ packages/default-ui/src/components/Balance.tsx
@@ -1,6 +1,8 @@
 import Text from "@/components/interface/typography/Text";
 import Container from "@/components/interface/layout/Container";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import sendIconUrl from "@/assets/icons/send.svg";
 
 type BalanceProps = {
     /** Set to true to shrink size of balance component */
@@ -51,25 +53,12 @@ export default function Balance({ shrink, balance, price, onSendClick }: Balance
             >{`$${fiatBalance || "0"} USD`}</Text.span>
 
             {/* MARK: Send button */}
-            <button
-                onClick={onSendClick}
+            <Link
+                to="/send"
                 className="absolute right-12 top-1/2 -translate-y-1/2 w-16 h-16 rounded-full bg-[color:#E2E2FE] text-purple-dark flex items-center justify-center"
             >
-                <svg
-                    width="25"
-                    height="25"
-                    viewBox="0 0 25 25"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                >
-                    <path
-                        fillRule="evenodd"
-                        clipRule="evenodd"
-                        d="M24.7532 1.82141C25.0646 1.01252 24.2696 0.2176 23.4607 0.52895L1.19844 9.09799C0.421019 9.39723 0.328845 10.4601 1.04315 10.8887L7.31814 14.6539C7.68221 14.8723 8.14455 14.837 8.47127 14.5659L14.3358 9.69888C15.2838 8.91217 16.5385 10.2016 15.7263 11.1277L10.7448 16.8081C10.4582 17.1349 10.4155 17.6093 10.6391 17.9819L14.3936 24.2392C14.8222 24.9535 15.8851 24.8613 16.1844 24.0839L24.7532 1.82141Z"
-                        className="fill-current"
-                    />
-                </svg>
-            </button>
+                <img src={sendIconUrl} width={25} height={25} alt="Send" />
+            </Link>
         </Container>
     );
 }
