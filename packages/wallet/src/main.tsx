8516dca8fbb0aee38b1915e1d7290ee2285013dc
Eyal Chojnowski <eyal.choj@gmail.com>
2022-05-30 14:14:30 +0200

    :arrow_up: (wallet) upgrade to react 18

--- packages/wallet/src/main.tsx
+++ packages/wallet/src/main.tsx
@@ -1,15 +1,17 @@
 import { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
 
 import "@/index.css";
 import App from "@/App";
 
-ReactDOM.render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+const root = createRoot(document.getElementById("root")!);
+
+root.render(
     <StrictMode>
         <HashRouter>
             <App />
         </HashRouter>
     </StrictMode>,
-    document.getElementById("root"),
 );
