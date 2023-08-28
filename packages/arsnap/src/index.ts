45e8490fe64d23d9c575a9a518d6386d650206d4
Eyal Chojnowski <eyal.choj@gmail.com>
2023-08-28 19:24:41 +0200

    :floppy_disk: arsnap: Rename the snap to Arweave Wallet

--- packages/arsnap/src/index.ts
+++ packages/arsnap/src/index.ts
@@ -85,13 +85,13 @@ const handler: RpcMessageHandler = async ({ origin, request }): Promise<RpcRespo
     const [maybeState, releaseState] = await getState();
 
     if (!maybeState) {
-        await notify("Initializing Arsnap, please allow up to 2 mins...");
+        await notify("Initializing Arweave Wallet, please allow up to 2 mins...");
     }
 
     const state = maybeState || (await initializeState());
 
     if (!maybeState) {
-        await notify("Arsnap is ready.");
+        await notify("Arweave Wallet is ready.");
     }
 
     let response;
